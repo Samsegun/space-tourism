13baa1f0de2f888ef7bae9e253ed0a0b9dcb01d2
Samsegun <oyebadesamsegun@gmail.com>
2022-01-31 22:19:39 +0100

    header added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./App.module.css";
+import Header from "./Components/UI/Header";
 
 function App() {
-  return <div className={styles.div}>hello</div>;
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,9 +1,46 @@
+import styles from "./Header.module.css";
+
 const Header = () => {
   return (
-    <header>
-      <div className="logo"></div>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <a href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
+            <g fill="none" fillRule="evenodd">
+              <circle cx="24" cy="24" r="24" fill="#FFF" />
+              <path
+                fill="#0B0D17"
+                d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
+              />
+            </g>
+          </svg>
+        </a>
+      </div>
 
-      <nav></nav>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            <a href="/">
+              <span>00</span> Home
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span>01</span> Destination
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span>02</span> Crew
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span>03</span> Technology
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
