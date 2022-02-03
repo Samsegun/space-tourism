a74db93e5fd7df24691bc5e43686b76e16926e8e
Samsegun <oyebadesamsegun@gmail.com>
2022-02-03 21:50:32 +0100

    styles for home background to be rewritten

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Main from "./main";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <>
+    <div className={styles["main-wrapper"]}>
       <Main />
-    </>
+    </div>
   );
 };
 
