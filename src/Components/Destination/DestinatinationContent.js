b267a86846e83eb22f1ebc14cf81ee6ca16fb676
Samsegun <oyebadesamsegun@gmail.com>
2022-02-09 15:35:15 +0100

    destination desktop mode

--- src/Components/Destination/DestinatinationContent.js
+++ src/Components/Destination/DestinatinationContent.js
@@ -1,11 +1,14 @@
 import styles from "./DestinationContent.module.css";
-import Img from "../../assets/destination/image-moon.png";
+// import Img from "../../assets/destination/image-moon.png";
 import data from "../../data.json";
+
+const destinations = data.destinations;
+console.log(destinations);
 
 const DestinationContent = () => {
   const loadData = () => {
-    console.log(data.destinations);
-    console.log(data.destinations[0].images.png);
+    console.log(destinations);
+    console.log(destinations[0].images.png);
   };
 
   return (
@@ -15,38 +18,44 @@ const DestinationContent = () => {
       </h2>
 
       <section>
-        <div className={styles["img-container"]} onClick={loadData}>
-          <img src={Img} alt="" />
-          {/* <img src={data.destinations[0].images.png} alt="" /> */}
+        <div className={styles["img-container"]}>
+          {/* <img src={Img} alt="" /> */}
+          <img
+            src={`${destinations[0].images.png}`}
+            alt={`${destinations[0].name}`}
+          />
         </div>
 
-        <ul className={styles.list}>
-          {data.destinations.map((data, idx) => (
-            <li
-              key={idx}
-              className={
-                data.name.toLowerCase() === "moon" ? styles.active : ""
-              }
-            >
-              {data.name}
-            </li>
-          ))}
-        </ul>
+        <div className={styles["content-container"]}>
+          <ul className={styles.list}>
+            {destinations.map((data, idx) => (
+              <li
+                key={idx}
+                className={
+                  data.name.toLowerCase() === "moon" ? styles.active : ""
+                }
+                onClick={loadData}
+              >
+                {data.name}
+              </li>
+            ))}
+          </ul>
 
-        <div className={styles["destination-details"]}>
-          <h1 className={styles.title}>{data.destinations[0].name}</h1>
+          <div className={styles["destination-details"]}>
+            <h1 className={styles.title}>{destinations[0].name}</h1>
 
-          <p className={styles.desc}>{data.destinations[0].description}</p>
-        </div>
+            <p className={styles.desc}>{destinations[0].description}</p>
+          </div>
 
-        <div className={styles["destination-estimates"]}>
-          <h2 className={styles.distance}>
-            avg. distance <span>{data.destinations[0].distance}</span>
-          </h2>
+          <div className={styles["destination-estimates"]}>
+            <h2 className={styles.distance}>
+              avg. distance <span>{destinations[0].distance}</span>
+            </h2>
 
-          <h2 className={styles.distance}>
-            est. travel time <span>{data.destinations[0].travel}</span>
-          </h2>
+            <h2 className={styles.distance}>
+              est. travel time <span>{destinations[0].travel}</span>
+            </h2>
+          </div>
         </div>
       </section>
     </main>
