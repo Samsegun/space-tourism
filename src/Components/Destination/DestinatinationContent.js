b90d4afcdfd8a3fc9b9510b28d123e7922284e28
Samsegun <oyebadesamsegun@gmail.com>
2022-02-09 16:52:07 +0100

    active states for destination added

--- src/Components/Destination/DestinatinationContent.js
+++ src/Components/Destination/DestinatinationContent.js
@@ -1,63 +1,14 @@
 import styles from "./DestinationContent.module.css";
-// import Img from "../../assets/destination/image-moon.png";
-import data from "../../data.json";
-
-const destinations = data.destinations;
-console.log(destinations);
+import Section from "./Section";
 
 const DestinationContent = () => {
-  const loadData = () => {
-    console.log(destinations);
-    console.log(destinations[0].images.png);
-  };
-
   return (
     <main className={styles.wrapper}>
       <h2 className={styles.intro}>
         <span>01</span> Pick your destination
       </h2>
 
-      <section>
-        <div className={styles["img-container"]}>
-          {/* <img src={Img} alt="" /> */}
-          <img
-            src={`${destinations[0].images.png}`}
-            alt={`${destinations[0].name}`}
-          />
-        </div>
-
-        <div className={styles["content-container"]}>
-          <ul className={styles.list}>
-            {destinations.map((data, idx) => (
-              <li
-                key={idx}
-                className={
-                  data.name.toLowerCase() === "moon" ? styles.active : ""
-                }
-                onClick={loadData}
-              >
-                {data.name}
-              </li>
-            ))}
-          </ul>
-
-          <div className={styles["destination-details"]}>
-            <h1 className={styles.title}>{destinations[0].name}</h1>
-
-            <p className={styles.desc}>{destinations[0].description}</p>
-          </div>
-
-          <div className={styles["destination-estimates"]}>
-            <h2 className={styles.distance}>
-              avg. distance <span>{destinations[0].distance}</span>
-            </h2>
-
-            <h2 className={styles.distance}>
-              est. travel time <span>{destinations[0].travel}</span>
-            </h2>
-          </div>
-        </div>
-      </section>
+      <Section />
     </main>
   );
 };
---
+++ src/Components/Destination/Section.js
@@ -0,0 +1,60 @@
+import styles from "./Section.module.css";
+import data from "../../data.json";
+import { useState } from "react";
+
+const destinations = data.destinations;
+
+const Section = () => {
+  const [active, setActive] = useState(0);
+
+  const loadData = Event => {
+    setActive(
+      destinations.findIndex(each => each.name === Event.target.textContent)
+    );
+  };
+
+  return (
+    <section>
+      <div className={styles["img-container"]}>
+        <img
+          src={`${destinations[active].images.png}`}
+          alt={`${destinations[active].name}`}
+        />
+      </div>
+
+      <div className={styles["content-container"]}>
+        <ul className={styles.list}>
+          {destinations.map((data, idx) => (
+            <li
+              key={idx}
+              className={
+                data.name === destinations[active].name ? styles.active : ""
+              }
+              onClick={loadData}
+            >
+              {data.name}
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles["destination-details"]}>
+          <h1 className={styles.title}>{destinations[active].name}</h1>
+
+          <p className={styles.desc}>{destinations[active].description}</p>
+        </div>
+
+        <div className={styles["destination-estimates"]}>
+          <h2 className={styles.distance}>
+            avg. distance <span>{destinations[active].distance}</span>
+          </h2>
+
+          <h2 className={styles.distance}>
+            est. travel time <span>{destinations[active].travel}</span>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Section;
