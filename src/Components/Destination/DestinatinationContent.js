f932affc91c8685d080d3f57472bfa796bbea840
Samsegun <oyebadesamsegun@gmail.com>
2022-02-23 16:03:09 +0100

    navbar style fixes

--- src/Components/Destination/DestinatinationContent.js
+++ src/Components/Destination/DestinatinationContent.js
@@ -6,7 +6,6 @@ const DestinationContent = () => {
   return (
     <main className={styles.wrapper}>
       <PageHeader text="01" intro="pick your destination" />
-
       <Section />
     </main>
   );
