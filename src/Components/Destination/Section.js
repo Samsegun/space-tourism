802fa34047164b90f36f3b811d202e30116b2077
Samsegun <oyebadesamsegun@gmail.com>
2022-03-17 15:19:36 +0100

    style fixes for destination page

--- src/Components/Destination/Section.js
+++ src/Components/Destination/Section.js
@@ -20,13 +20,13 @@ const Section = () => {
         <motion.img
           src={`${destinations[active].images.png}`}
           alt={`${destinations[active].name}`}
-          drag
-          dragConstraints={{
-            left: 10,
-            right: 20,
-            bottom: 10,
-            top: 5,
-          }}
+          // drag
+          // dragConstraints={{
+          //   left: 10,
+          //   right: 20,
+          //   bottom: 10,
+          //   top: 5,
+          // }}
         />
       </div>
 
